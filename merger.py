9a9013d7bf1624bbc60e4a0a4d0b327831097395
gmc314 <grant.cheng1@gmail.com>
2024-01-18 14:02:46 -0500

    fix read file from directory

--- merger.py
+++ merger.py
@@ -16,11 +16,17 @@ pdf_directory = os.fsdecode(pdf_directory_str)
 # 4. initialize a merger
 merger = PyPDF2.PdfFileMerger()
 
-# 5. iterate through the pdfs
-for pdf in os.listdir(pdf_directory):
-    
-    # 6. add pdf to merger
-    merger.append(pdf)
+# 5. get a list of pdf files in specified directory
+pdf_files = os.listdir(pdf_directory)
 
-# 6. write the new merged pdf file
+# 6. iterate through the pdfs
+for pdf in pdf_files:
+
+    # 7. get pdf from its directory
+    pdf_content = PyPDF2.PdfFileReader(os.path.join(pdf_folder, pdf), 'rb')
+
+    # 8. add pdf to merger
+    merger.append(pdf_content)
+
+# 9. write the new merged pdf file
 merger.write(f'{name_of_merged_file}.pdf')
